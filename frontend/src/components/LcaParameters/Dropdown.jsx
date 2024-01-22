c3e8bbe101a45decaecd66fa6ae754c889b55247
RenisSh <shutina.renis62@gmail.com>
2024-01-22 15:27:17 +0100

    made a test dropdown that works, needs some fixes to be fully functional

--- frontend/src/components/LcaParameters/Dropdown.jsx
+++ frontend/src/components/LcaParameters/Dropdown.jsx
@@ -1,12 +1,31 @@
+import {useState} from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCaretDown } from '@fortawesome/free-solid-svg-icons'
+
+
 function Dropdown(){
+    const[isActive, setIsActive] = useState(false);
 return (
 <div className="dropdown">
-<div className="dropdown-btn"> Choose name: </div>
-    <div className="dropdown-content">  
-        <div className="dropdown-list">
-        React
-        </div> 
-    </div>
+<div className="dropdown-btn" 
+onClick={ e=> setIsActive(!isActive)}
+onBlur={() => setIsActive(false)}
+tabIndex="0"
+>
+ Choose One 
+ <FontAwesomeIcon icon={faCaretDown} />
+</div>
+    {isActive && ( 
+        <div className="dropdown-content">  
+            <div className="dropdown-item">
+                React
+            </div> 
+            <div className="dropdown-item">
+                Angular
+            </div> 
+        </div>
+    )}
+   
 </div>
 );
 }
