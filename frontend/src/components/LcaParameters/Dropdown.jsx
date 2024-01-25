3313e37e837e1de241c632b6ad88b86e0df25418
alexey192 <alexey192@gmail.com>
2024-01-25 10:45:36 +0100

    get product systems api call

--- frontend/src/components/LcaParameters/Dropdown.jsx
+++ frontend/src/components/LcaParameters/Dropdown.jsx
@@ -1,10 +1,12 @@
 import {useState} from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCaretDown } from '@fortawesome/free-solid-svg-icons'
+//import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+//import { faCaretDown } from '@fortawesome/free-solid-svg-icons'
 
 
-function Dropdown(){
+function Dropdown({select,setSelected}, options){
     const[isActive, setIsActive] = useState(false);
+    const abstractDrivers = ['Delivery Vehicle', 'Packaging', 'Routes', 'Filling Material']
+    //const abstractDrivers = options
 return (
 <div className="dropdown">
 <div className="dropdown-btn" 
@@ -13,16 +15,17 @@ onBlur={() => setIsActive(false)}
 tabIndex="0"
 >
  Choose One 
- <FontAwesomeIcon icon={faCaretDown} />
+ {/* <FontAwesomeIcon icon={faCaretDown} /> */}
 </div>
     {isActive && ( 
         <div className="dropdown-content">  
-            <div className="dropdown-item">
-                React
-            </div> 
-            <div className="dropdown-item">
-                Angular
-            </div> 
+        {abstractDrivers.map((option) => (<div className="dropdown-item"
+            onClick={e => {
+            setSelected(option)
+            setIsActive(false)}
+            }>
+                {option}
+            </div> ))}
         </div>
     )}
    
--- frontend/src/components/LcaParameters/LcaParameters.jsx
+++ frontend/src/components/LcaParameters/LcaParameters.jsx
@@ -3,10 +3,12 @@ import { Flex, Heading, Card, CardHeader, CardBody, Text, Select, Stack, Button,
 import { FiChevronDown } from 'react-icons/fi';
 import Dropdown from './Dropdown';
 import "./styles.css"
+import { faRightFromBracket } from "@fortawesome/free-solid-svg-icons";
 
 const LcaParameters = () => {
     const [inputValue, setInputValue] = useState('');
     const [apiUrl, setApiUrl] = useState('');
+    const impactMethodId = 'b4571628-4b7b-3e4f-81b1-9a8cca6cb3f8';
   
     const handleInputChange = (event) => {
       setInputValue(event.target.value);
@@ -43,86 +45,97 @@ const LcaParameters = () => {
 
     const[select, setSelected] = useState("Choose Abstract Component");
     const[selectC, setSelectedC] = useState("Choose Concrete Component");
-  
+    const [inputName, setInputName] = useState('');
     return (
 <div>
 
 
-        <label
+      <label
         style={{
-            padding: '20px 5px',
-            fontSize: '25px',
-            fontWeight: 'bold',
-            }}>
-Fetch OpenLCA data
-        </label>
-        <div style={{ display: 'flex', alignItems: 'center' }}>
-        <div style={{ 
-  display: 'flex', 
-  alignItems: 'center', 
-  border: '0.1px solid black', /* This will add a border */
-  borderRadius: '8px', /* This will make the border round angled */
-  padding: '0px', /* This will add some space between the border and the elements inside */
-}}>
-  <label
-style={{
-    backgroundColor: 'default', /* Teal */
-    border: 'none',
-    color: 'black',
-    padding: '10px 25px',
-    textAlign: 'center',
-    textDecoration: 'none',
-    display: 'inline-block',
-    fontSize: '15px',
-    fontWeight: 'bold',
-    margin: '2px 1px',
-    cursor: 'default',
-    borderRadius: '8px',
-  }}
->
-Enter URL:  
-</label>
-  <input
-  style={{
-    padding: '5px',
-  }}
-    type="text"
-    value={apiUrl}
-    onChange={handleApiUrlChange}
-    placeholder="Enter API URL..."
-  />
-</div>
-
-          <button 
-            onClick={handleButtonClick} 
+          padding: '20px 5px',
+          fontSize: '25px',
+          fontWeight: 'bold',
+        }}>
+        Fetch OpenLCA data
+      </label>
+      <div style={{ display: 'flex', alignItems: 'center' }}>
+        <div style={{
+          display: 'flex',
+          alignItems: 'center',
+          border: '0.1px solid black', /* This will add a border */
+          borderRadius: '8px', /* This will make the border round angled */
+          padding: '0px', /* This will add some space between the border and the elements inside */
+        }}>
+          <label
             style={{
-              backgroundColor: '#E8E8E8',
-              border: '0.1px solid black',
+              backgroundColor: 'default', /* Teal */
+              border: 'none',
               color: 'black',
-              padding: '12px 40px',
+              padding: '10px 25px',
               textAlign: 'center',
               textDecoration: 'none',
               display: 'inline-block',
               fontSize: '15px',
               fontWeight: 'bold',
-              margin: '25px 10px',
-              cursor: 'pointer',
+              margin: '2px 1px',
+              cursor: 'default',
               borderRadius: '8px',
             }}
           >
-            Fetch
-          </button>
+            Enter URL:
+          </label>
+          <input
+            style={{
+              padding: '5px',
+            }}
+            type="text"
+            value={apiUrl}
+            onChange={handleApiUrlChange}
+            placeholder="e.g., http://localhost:8080"
+          />
         </div>
+
+        <button
+          onClick={handleButtonClick}
+          style={{
+            backgroundColor: '#E8E8E8',
+            border: '0.1px solid black',
+            color: 'black',
+            padding: '12px 40px',
+            textAlign: 'center',
+            textDecoration: 'none',
+            display: 'inline-block',
+            fontSize: '15px',
+            fontWeight: 'bold',
+            margin: '25px 10px',
+            cursor: 'pointer',
+            borderRadius: '8px',
+          }}
+        >
+          Fetch
+        </button>
+      </div>
 
 
         <hr style={{borderTop: '2px solid black'}}/>
         <br></br>
         
-      
+        {variants.map((item, index) => (
+        <div key={index}
+        style={{
+          border: '0.1px solid black',
+              padding: '2px',
+              margin: '2px',
+              fontWeight: 'bold',
+              fontSize: '15px'
+        }}>
+        {item}</div> // Create a new div for each item
+      ))}
       <div className="Big-Container" style={{ 
               border: '0.1px solid black',
               padding: '0px',
-              margin: '0px',}}>
+              margin: '0px',
+              }}>
                                 <div style={{  /* Here starts the first activity configuration */
                                                               display: 'flex', 
                                                               alignItems: 'center', 
@@ -135,43 +148,58 @@ Enter URL:
                                                     }}>
                                 Activity 1
                                         </label>
-                                <Dropdown selected={select} setSelected={setSelected} />
-                                <Dropdown selected={select} setSelected={setSelected} />
+                                <Dropdown selected={select} setSelected={setSelected}/>
+                                <Dropdown selected={selectC} setSelected={setSelectedC} />
                                 </div> 
 
-                                <div style={{  /* Here starts the second activity configuration */
-                                  display: 'flex', 
-                                  alignItems: 'center', 
-                                  /* Here ends the second activity configuration */
-                                                    
-                                }}>
-                                  <label style={{
-                                                        padding: '5px 10px',
-                                                        fontSize: '15px',
-                                                        fontWeight: 'bold',
-                                                    }}>
-                                Activity 2
-                                        </label>
-                                <Dropdown selected={select} setSelected={setSelected} />
-                                <Dropdown selected={select} setSelected={setSelected} />
-                                </div>
+        <div style={{  /* Here starts the second activity configuration */
+          display: 'flex',
+          alignItems: 'center',
+          /* Here ends the second activity configuration */
 
-                                <div style={{  /* Here starts the third activity configuration */
-                                  display: 'flex', 
-                                  alignItems: 'center', 
-                                  /* Here ends the third activity configuration */
-                                }}>
-                                  <label style={{
-                                                        padding: '5px 10px',
-                                                        fontSize: '15px',
-                                                        fontWeight: 'bold',
-                                                    }}>
-                                Activity 3
-                                        </label>
-                                <Dropdown selected={select} setSelected={setSelected} />
-                                <Dropdown selected={select} setSelected={setSelected} />
-                                </div>
+        }}>
+          <label style={{
+            padding: '5px 10px',
+            fontSize: '15px',
+            fontWeight: 'bold',
+          }}>
+            Activity 2
+          </label>
+          <Dropdown selected={select} setSelected={setSelected} />
+          <Dropdown selected={select} setSelected={setSelected} />
+        </div>
 
+        <div style={{  /* Here starts the third activity configuration */
+          display: 'flex',
+          alignItems: 'center',
+          /* Here ends the third activity configuration */
+        }}>
+          <label style={{
+            padding: '5px 10px',
+            fontSize: '15px',
+            fontWeight: 'bold',
+          }}>
+            Activity 3
+          </label>
+          <Dropdown selected={select} setSelected={setSelected} />
+          <Dropdown selected={select} setSelected={setSelected} />
+        </div>
+
+        <div style={{  /* Here starts the fourth activity configuration */
+          display: 'flex',
+          alignItems: 'center',
+          /* Here ends the fourth activity configuration */
+        }}>
+          <label style={{
+            padding: '5px 10px',
+            fontSize: '15px',
+            fontWeight: 'bold',
+          }}>
+            Activity 4
+          </label>
+          <Dropdown selected={select} setSelected={setSelected} />
+          <Dropdown selected={select} setSelected={setSelected} />
+        </div>
                                 <div style={{  /* Here starts the fourth activity configuration */
                                   display: 'flex', 
                                   alignItems: 'center', 
@@ -187,12 +215,37 @@ Enter URL:
                                 <Dropdown selected={select} setSelected={setSelected} />
                                 <Dropdown selected={select} setSelected={setSelected} />
                                 </div>
+              <div style={{ display: 'flex', justifyContent: 'flex-end'}}>
+              <input style={{
+                display: 'flex',
+                    marginLeft: 'auto',
+                    border: '0.1px solid black',
+                    padding: '5px',
+                    margin:'5px',
+              }}
+                type="text" 
+                value={inputName}
+                placeholder="Save Variant as:" 
+                onChange={e => setInputName(e.target.value)} // Update inputName when the input changes
+              />
+                  <button style={{
+                    margin: '5px',
+                    padding: '5px',
+                    fontWeight: 'bold',
+                    background: '#23b131',
+                    border: '0.1px solid black',
+                    cursor: 'pointer',
+                    borderRadius: '8px',
+                  }} 
+                  onClick={handleSaveButtonClick}>
+                    Save variant
+                  </button>
+            </div>
       </div>
 
 
 
-
-</div>  
-    );
-  };
+    </div>
+  );
+};
 export default LcaParameters;
